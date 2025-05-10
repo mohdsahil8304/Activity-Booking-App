e5a300693eed6cddae8e0644a7eb2129578241b1
MOHD SAHIL <97383346+mohdsahil8304@users.noreply.github.com>
2025-05-10 12:23:14 +0530

    update activityRoutes

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { listActivities } = require('../controllers/activityController');
 const router = express.Router();
-const { check } = require('express-validator');
 
 // const {Activity }= require('../models/activity');
 
